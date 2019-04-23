2a31858c3db7d7c91b5f033e935d0fd5e2221467
Yash Turkar <yashturkar@gmail.com>
2019-04-23 23:06:36 +0530

    my code

--- Own_Algo.py
+++ Own_Algo.py
@@ -2,87 +2,51 @@ import numpy as np
 import socket as sk
 import re
 import struct
+import time
 
 print("Enter message")
-
 plain_text = input()
-
+# Pad PT if small
 padding = len(plain_text)%3
-
 if padding != 0:
     plain_text = plain_text+('x'*(padding-1))
     print ('padded plain text = '+plain_text)
-
+# Split PT in sets
 pairs = re.findall('...',plain_text)
-
 print('Sets after splitting = ',pairs)
-
+# Put Characters into matrices
 equationOne_String = pairs[0]
-
 equationTwo_String = pairs[1]
-
 equationOne_LHS = [ord(equationOne_String[0])-96,ord(equationOne_String[1])-96]
-
 equationTwo_LHS = [ord(equationTwo_String[0])-96,ord(equationTwo_String[1])-96]
-
 equationOne_RHS = ord(equationOne_String[2])-96
-
 equationTwo_RHS = ord(equationTwo_String[2])-96
-
 matrix_LHS = np.array([equationOne_LHS,equationTwo_LHS])
-
 matrix_RHS = np.array([equationOne_RHS,equationTwo_RHS])
-
+# Solve Simultaneous equations
 simul_output = np.linalg.solve(matrix_LHS,matrix_RHS)
-
 X_Value = simul_output[0]%26
-
 Y_Value = simul_output[1]%26
-
-''''''
-print(matrix_LHS)
-
-print(matrix_RHS)
-
-print(X_Value)
-
-print(Y_Value)
-''''''
-
+# Calculate Key
 key = (X_Value+Y_Value)%26
-
 print ('Key is ',key)
-
+# Encrypt
 cipher_text = []
-
 for x in plain_text:
     cipher_text.append(chr(ord(x)^int(key)))
-
 cipher_text_final = ''
-
 cipher_text_final =  cipher_text_final.join(cipher_text)
-
 print (cipher_text)
 print (cipher_text_final)
-
+# Send Cipher Text and Key
 s = sk.socket()
-
 host = sk.gethostname()
-
 port = 12345
-
 s.connect((host,port))
-
 key = int(key)
-
 s.send(str(key).encode())
-
-import time
-
 time.sleep(1)
-
 s.send(cipher_text_final.encode())
-
 s.close()
 
 
--- Own_Algo_Client.py
+++ Own_Algo_Client.py
@@ -1,45 +1,30 @@
 import socket as sk
 import struct
-
-#key will be encrypted using another key sent by diffie hellman
-#for this example , key will be sent directly
-
-s = sk.socket()
-
-host = sk.gethostname()
-
-port = 12345
-
-s.bind((host,port))
-
-s.listen(5)
-
-c, addr = s.accept()
-
-key = c.recv(1024)
-
-key = int(key)
-
-print(key)
-
 import time
+# key will be encrypted using another key sent by diffie hellman
+# for this example , key will be sent directly
+s = sk.socket()
+host = sk.gethostname()
+port = 12345
+s.bind((host,port))
+s.listen(5)
+c, addr = s.accept()
+#Receive key
+key = c.recv(1024)
+key = int(key)
+print(key)
+# Delay to receive next packet
 time.sleep(1)
-
+# Receive Cipher Text
 cipher_text = (c.recv(1024)).decode()
-
 print (cipher_text)
 
+# Decrypt Cipher text
 decodedText = []
-
 for y in cipher_text:
     decodedText.append(chr(ord(y)^int(key)))
-
 print (decodedText)
-
 decodedText_final = ''
-
 decodedText_final = decodedText_final.join(decodedText)
-
 print (decodedText_final)
-
 s.close()
